9fc4e3a631de07876b4a3bf47357aa001584db90
duncanmayer <duncmayer@gmail.com>
2025-04-05 13:40:00 -0400

    commiting these to newA6

--- Kambaz/Database/index.js
+++ Kambaz/Database/index.js
@@ -1,7 +1,7 @@
-import courses from "./courses.js";
-import modules from "./modules.js";
-import assignments from "./assignments.js";
-import users from "./users.js";
-import grades from "./grades.js";
-import enrollments from "./enrollments.js";
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+import grades from "./grades.json";
+import enrollments from "./enrollments.json";
 export default { courses, modules, assignments, users, grades, enrollments };
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: "http://localhost:5173",
   })
 );
 const sessionOptions = {
